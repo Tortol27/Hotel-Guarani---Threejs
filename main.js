cbceb16c816b6c4d5198c64837351be19d4b34c7
Tortol27 <jose.alarcon.dentice@gmail.com>
2024-01-15 11:12:46 -0300

    Add texturas y lighting/shadows

--- main.js
+++ main.js
@@ -8,9 +8,8 @@ class PrismGeometry extends THREE.ExtrudeGeometry {
   }
 }
 
-let container, stats;
-
 let camera, scene, renderer;
+let a = -3000;
 
 let particleLight;
 let group;
@@ -34,56 +33,94 @@ function init() {
         new THREE.MeshBasicMaterial( { color: 0xffffff } )
     );
     scene.add( particleLight );
-
-    particleLight.add( new THREE.PointLight( 0xffffff, 90 ) );
+    var light = new THREE.DirectionalLight( 0xffffff, 4 );
+    light.castShadow = true;
+    // particleLight.add( new THREE.PointLight( 0xffffff, 90 ) );
+    particleLight.add (light);
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.shadowMap.enabled= true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+
+    var light2 = new THREE.AmbientLight(0xffffff,1);
+    group.add(light2);
 
     // Terraza
     geometry = new THREE.BoxGeometry(2.3,0.05,2.3);
     geometry.translate(-0.15,-1.4,0.9);
-    material = new THREE.MeshToonMaterial( { color: 0x787878 } );
+    const textureCemento = new THREE.TextureLoader().load('textures/cemento.jpg' );
+    textureCemento.wrapS = THREE.RepeatWrapping;
+    textureCemento.wrapT = THREE.RepeatWrapping;
+    textureCemento.repeat.x = 4;
+    textureCemento.repeat.y = 4;
+    textureCemento.repeat.z = 4;
+    material = new THREE.MeshToonMaterial( { color: 0xBCBDB7, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
     
     // 1er piso
     geometry = new THREE.BoxGeometry(1.9,0.2,1.9);
     geometry.translate(-0.15,-1.5,0.85);
     material = new THREE.MeshToonMaterial( { color: 0x00278 } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     // Planta baja central
     geometry = new THREE.BoxGeometry(1.1,0.2,0.8); 
     geometry.translate(0.13,-1.7,0.9);
     material = new THREE.MeshToonMaterial( { color: 0xaa0078 } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     geometry = new THREE.BoxGeometry(0.4,0.2,0.8); // Planta baja izq
     geometry.translate(-0.78,-1.7,0.9);
     material = new THREE.MeshToonMaterial( { color: 0xffffff } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     geometry = new THREE.BoxGeometry(1.9,0.2,0.46); // Planta baja fondo
     geometry.translate(-0.15,-1.7,0.13);
     material = new THREE.MeshToonMaterial( { color: 0x00bc78 } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     geometry = new THREE.ConeGeometry(0.45,0.15,3,1) // Tragaluz
     geometry.translate(-0.85,-1.3,0.85);
-    material = new THREE.MeshToonMaterial( { color: 0xff1000 } );
+    const textureVidrio2 = new THREE.TextureLoader().load('textures/glass2.jpg' );
+    textureVidrio2.wrapS = THREE.RepeatWrapping;
+    textureVidrio2.wrapT = THREE.RepeatWrapping;
+    textureVidrio2.repeat.x = 4;
+    material = new THREE.MeshToonMaterial( { color: 0x3dd3eb, map: textureVidrio2 } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     geometry = new THREE.BoxGeometry(0.7,0.01,0.55); // Piscina
     geometry.translate(-0.65,-1.37,1.6);
     material = new THREE.MeshToonMaterial( { color: 0xff110 } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     // Pilares
@@ -128,8 +165,12 @@ function init() {
     var pillarGeometry = new THREE.BufferGeometry();
     pillarGeometry.setIndex(indices);
     pillarGeometry.setAttribute('position',new THREE.BufferAttribute(pillarVertices, 3));
-    material = new THREE.MeshToonMaterial( { color: 0x606060 } );
+    
+    material = new THREE.MeshToonMaterial( { color: 0xBCBDB7, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     var mesh = new THREE.Mesh(pillarGeometry, material);
+    mesh.receiveShadow = true;
+    mesh.castShadow = true;
     
     // Translate, rotate y group.add las veces que se repita el pilar
     pillarGeometry.rotateY(-Math.PI / 2);
@@ -177,7 +218,10 @@ function init() {
     geometry.translate(0.1,-1.05,0.15);
     // geometry.rotateY(Math.PI)
     var material = new THREE.MeshToonMaterial( { color: 0xFFBF00 } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add( cube );
 
     var A = new THREE.Vector3( 0.2, 0.7, 0.2);
@@ -189,14 +233,20 @@ function init() {
     geometry.rotateX(Math.PI/2);
     // geometry.scale(1.05,1.05,1.05);
     geometry.translate(0.1,-1.2,0.17);
-    var material = new THREE.MeshToonMaterial( { color: 0x78e667 } );
+    var material = new THREE.MeshToonMaterial( { color: 0x3b5178, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add( cube );
 
     var geometry = new THREE.BoxGeometry(0.4,0.35,0.1); // Base rectangular de atras
-    geometry.translate(0.08,-1.23,0.15);
-    material = new THREE.MeshToonMaterial( { color: 0xb0171c } );
+    geometry.translate(0.1,-1.23,0.15);
+    material = new THREE.MeshToonMaterial( { color: 0x3b5178, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube);
 
     // Pilares de Habitaciones
@@ -205,8 +255,11 @@ function init() {
     var pilar = new THREE.BufferGeometry();
     pilar.setIndex(indices);
     pilar.setAttribute('position',new THREE.BufferAttribute(pillarVertices, 3));
-    material = new THREE.MeshToonMaterial( { color: 0xe36895 } );
+    material = new THREE.MeshToonMaterial( { color: 0xBCBDB7, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     var meshH = new THREE.Mesh(pilar, material);
+    meshH.receiveShadow = true;
+    meshH.castShadow = true;
     pilaresH.scale.set(0.8, 0.8,0.8);
     pilaresH.rotateY(1.064);
     pilaresH.position.set(-1.43, 0.062,1.05);
@@ -251,11 +304,26 @@ function init() {
     var C2 = new THREE.Vector3(-0.4 ,0, 0.2 );
     var height = 0.1;                   
     var geometry = new PrismGeometry( [ A1, B1,B2,C2,C1,A2 ], height ); // utilizo para crear el prisma rectangular
-    geometry.rotateX(Math.PI/2)
-    geometry.scale(1.35,1.35,1.35)
-    geometry.translate(2.1,0.29,0.1)
-    var material = new THREE.MeshToonMaterial( { color: 0x7ea0a6 } );
+    geometry.rotateX(Math.PI/2);
+    geometry.scale(1.35,1.35,1.35);
+    geometry.translate(2.1,0.29,0.1);
+    const textureVentanas = new THREE.TextureLoader().load('textures/test_ventanas.jpg' );
+    textureVentanas.wrapS = THREE.RepeatWrapping;
+    textureVentanas.wrapT = THREE.RepeatWrapping;
+    textureVentanas.repeat.x = 7;
+    textureVentanas.repeat.y = 4;
+    textureVentanas.repeat.z = 4;
+    // clearcoat: 1.0,
+    // clearcoatRoughness: 0.1,
+    // metalness: 0.9,
+    // roughness: 0.5,
+    // color: 0x0000ff,
+    // normalMap: normalMap3,
+    var material = new THREE.MeshToonMaterial( { color: 0x757980, map: textureVentanas } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     piso.add( cube );
 
     // Parte piso y balconcitos
@@ -268,26 +336,41 @@ function init() {
     geometry.scale(1.35,1.35,1.35);
     geometry.translate(2.1,0.155,0.1);
     // geometry.rotateY(Math.PI)
-    var material = new THREE.MeshToonMaterial( { color: 0xe0b9b6 } );
+    var material = new THREE.MeshToonMaterial( { color: 0xBCBDB7, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     piso.add( cube );
 
     // Punta de vidrio
     var A = new THREE.Vector3( 0, 1, 0.2);
-    var height = 0.12;                   
+    var height = 0.112;                   
     var geometry = new PrismGeometry( [ A, A1,A2 ], height ); // utilizo para crear el prisma rectangular
     geometry.rotateX(Math.PI/2);
     geometry.scale(1.35,1.35,1.35);
     geometry.translate(2.1,0.29,0.1);
     // geometry.rotateY(Math.PI)
-    var material = new THREE.MeshToonMaterial( { color: 0x3dd3eb } );
+    const textureVidrio = new THREE.TextureLoader().load('textures/glass.jpg' );
+    textureVidrio.wrapS = THREE.RepeatWrapping;
+    textureVidrio.wrapT = THREE.RepeatWrapping;
+    textureVidrio.repeat.x = 7;
+    textureVidrio.repeat.y = 4;
+    textureVidrio.repeat.z = 4;
+    var material = new THREE.MeshToonMaterial( { color: 0x3dd3eb, map: textureVidrio } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     piso.add( cube );
 
     var geometry = new THREE.BoxGeometry(0.4,0.165,0.05); // Parte rectangular de atras
     geometry.translate(2.1,0.207,0.1);
-    material = new THREE.MeshToonMaterial( { color: 0xb0171c } );
+    material = new THREE.MeshToonMaterial( { color: 0x3b5178, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     piso.add(cube);
 
     // Mutiplicar pisos
@@ -311,9 +394,11 @@ function init() {
     geometry.rotateX(Math.PI/2);
     geometry.scale(1.35,1.35,1.35);
     geometry.translate(0.1,0.342,0.1);
-    // geometry.rotateY(Math.PI)
-    var material = new THREE.MeshToonMaterial( { color: 0x1ee664 } );
+    var material = new THREE.MeshToonMaterial( { color: 0xBCBDB7, map: textureCemento } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     var cube2 = cube.clone();
     cube2.translateY(0.14);
     group.add( cube );
@@ -327,16 +412,21 @@ function init() {
     geometry.rotateX(Math.PI/2)
     geometry.scale(1,1,1);
     geometry.translate(0.1,0.45,0.2);
-    // geometry.rotateY(Math.PI)
     var material = new THREE.MeshToonMaterial( { color: 0xe6e047 } );
+    material.shading = THREE.SmoothShading;
     var cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+    cube.castShadow = true;
     group.add(cube)
 
     // Calle
     geometry = new THREE.BoxGeometry(5,0.00001,5);
     geometry.translate(-0.15,-1.81,0.9);
     material = new THREE.MeshToonMaterial( { color: 0xed7621 } );
+    material.shading = THREE.SmoothShading;
     cube = new THREE.Mesh(geometry, material);
+    cube.receiveShadow = true;
+    // cube.castShadow = true;
     group.add(cube);
 
     // EVENTS
@@ -372,10 +462,13 @@ function animate() {
 
 function render() {
 
-    const timer = Date.now() * 0.000025;
-    particleLight.position.x = 4 * Math.sin( timer * 2 );
-    particleLight.position.y = 4 * Math.cos( timer * 2 );
-
+    var timer = a * 0.00025;
+    particleLight.position.x = 17 * Math.sin( timer * 2 );
+    particleLight.position.y = 17 * Math.cos( timer * 2 );
+    a+= 1;
+    if (a >= 3000){
+        a = a*-1;
+    }
     // for ( let i = 0; i < group.children.length; i ++ ) {
 
     //     const child = group.children[ i ];
